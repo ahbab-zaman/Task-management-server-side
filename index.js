91a3c8d3f3436bafc59fc3fd0633b098581d554c
Ahbabuzzaman <160356472+ahbab-zaman@users.noreply.github.com>
2025-02-20 16:36:54 +0600

    create the APIs for get, post, delete, put

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 4000;
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.73pqt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -16,6 +17,8 @@ const client = new MongoClient(uri, {
   },
 });
 
+const tasksCollection = client.db("tasksDB").collection("tasks");
+
 async function run() {
   try {
     // await client.connect();
@@ -23,6 +26,37 @@ async function run() {
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
+
+    app.post("/task", async (req, res) => {
+      const query = req.body;
+      const result = await tasksCollection.insertOne(query);
+      res.send(result);
+    });
+
+    app.get("/tasks", async (req, res) => {
+      const result = await tasksCollection.find().toArray();
+      res.send(result);
+    });
+    app.put("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedTask = req.body;
+      const query = {
+        $set: {
+          name: updatedTask.name,
+          category: updatedTask.category,
+        },
+      };
+      const result = await tasksCollection.updateOne(filter, query);
+      res.send(result);
+    });
+
+    app.delete("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const cursor = { _id: new ObjectId(id) };
+      const result = await tasksCollection.deleteOne(cursor);
+      res.send(result);
+    });
   } finally {
     // await client.close();
   }
